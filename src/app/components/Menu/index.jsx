7ad1a4d09ea50ca1343af9d18713bbbe4f413f8c
FhucZoo <91193021+NgoTranBaoPhuc@users.noreply.github.com>
2023-12-10 14:28:46 +0700

    add secondMenu

--- src/app/components/Menu/index.jsx
+++ src/app/components/Menu/index.jsx
@@ -2,11 +2,13 @@
 import * as React from "react";
 import { styled } from "@mui/material";
 import FisrtMenu from "./FisrtMenu"
+import SecondMenu from "./SecondMenu"
 
 export default function Menu() {
     return (
         <div>
             <FisrtMenu />
+            <SecondMenu />
         </div>
     );
-  }
+}
